c28ef37331f8cf24f0a5179c975fe133460f3d1a
jayanthkumar9915623 <41735077+jayanthkumar9915623@users.noreply.github.com>
2018-07-27 14:09:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function loadjson(file,calback) {
+	var xhr=new XMLHttpRequest();
+	xhr.overrideMimeType("application/json");
+	xhr.open("GET",file,true);
+	xhr.onreadystatechange=function() {
+		if (xhr.readyState ===4 && xhr.status=="200") {
+			calback(xhr.responseText);
+		}
+	}
+	xhr.send();
+}
+loadjson("data.json",function(text){
+	let data=JSON.parse(text);
+	console.log(data);
+})
